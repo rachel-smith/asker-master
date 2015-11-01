9926bfdaa0e33d880c9b8a39ce2d2915e10955f9
Rachel Smith <smithra17@up.edu>
2015-11-01 11:46:13 -0800

    Added Model View Controller Files

---
+++ utility.js
@@ -0,0 +1,17 @@
+/*
+ * viz.js
+ * 
+ * Defines:
+ * -Data to be visualized in chart.
+ * -Options for look of the chart.
+ * -How the chart is to be drawn.
+ * 
+ * @author Team Yoyodyne
+ * @since Nov 1, 2015
+ */
+
+var librs = librs || {};
+
+google.load('visualization', '1', {packages: ['corechart']});
+
+
