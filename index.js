71add1e0a49345a9f664a092d4df2d6bbd9368ac
Rachel Smith <smithra17@up.edu>
2015-12-08 10:57:44 -0800

    Fixed heroku stuff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
 
-app.listen(app.get('port'), function()){
+app.listen(app.get('port'), function(){
 	console.log("Node is running at: " + app.get('port'));
-}
+});
 
